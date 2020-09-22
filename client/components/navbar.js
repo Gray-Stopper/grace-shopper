31f3f9ce08d9081075dd43e6290734fb288a2fc9
Melissa <66856935+mlam0527@users.noreply.github.com>
2020-09-22 18:03:21 -0400

    Navbar/Login

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,31 +3,74 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Login, Signup} from './auth-form'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>GRAY STOPPER</h1>
-    <h4>A virtual spa for your aging head</h4>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+class Navbar extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      showLoginForm: false,
+      showSignUpForm: false
+    }
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleSignUpClick = this.handleSignUpClick.bind(this)
+  }
+
+  handleLoginClick() {
+    this.setState(prevState => ({
+      showLoginForm: !prevState.showLoginForm,
+      showSignUpForm: false
+    }))
+  }
+
+  handleSignUpClick() {
+    this.setState(prevState => ({
+      showSignUpForm: !prevState.showSignUpForm,
+      showLoginForm: false
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>GRAY STOPPER</h1>
+        <h4>A virtual spa for your aging head</h4>
+        <nav>
+          {this.props.isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={this.props.handleClick}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <button
+                onClick={() => {
+                  this.handleLoginClick()
+                }}
+              >
+                Login
+              </button>
+              <button
+                onClick={() => {
+                  this.handleSignUpClick()
+                }}
+              >
+                Sign Up
+              </button>
+            </div>
+          )}
+        </nav>
+        {this.state.showLoginForm ? <Login /> : ''}
+        {this.state.showSignUpForm ? <Signup /> : ''}
+        <hr />
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
