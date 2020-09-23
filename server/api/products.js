912583210f23456b60428f3acb62bfa2dc3e993a
Melissa <66856935+mlam0527@users.noreply.github.com>
2020-09-23 11:17:31 -0400

    created prouduct route js file for all and single user

---
+++ server/api/products.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+module.exports = router
+
+//api route /api/products
+router.get('/', async (req, res, next) => {})
